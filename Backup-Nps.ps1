b0218fb6faad39aaac0985e034f26125c064e436
d3ade <d3ade@DESKTOP-VH8CG7N>
2019-04-21 22:28:22 -0500

    Script is ready for production and initial load

---
+++ Backup-Nps.ps1
@@ -0,0 +1,34 @@
+﻿#----------------------------------------------------------------------------------------------
+# Script: Backup-NPS.ps1
+# Author: Matthew Badali
+# Date: 04/21/2019 
+# Comments:
+#---------------------------------------------------------------------------------------------
+
+### Parameters
+Param(
+
+   [Parameter(Mandatory=$true)]
+   [ValidateScript({Test-Path $_ -PathType ‘Container’})]
+   [string]$BackupDest,
+
+   [string]$ComputerName = $env:COMPUTERNAME
+
+) #end param
+
+$date=(get-date).ToString("yyyy-MM-dd")
+$BackupPath ="$($BackupDest)\$($ComputerName)\"
+$BackupFileName = "$($BackupPath)$($ComputerName)-$($date)-NPSExport.xml"
+if (!(Test-Path $BackupPath)) {new-item $BackupPath -ItemType Directory}
+
+Export-NpsConfiguration -path $BackupFileName
+
+##########Clean  up backups older than 7 days##############
+
+
+$days ="-7"
+$daysback= (Get-Date).AddDays($days)
+(Get-ChildItem -Path $BackupPath -Recurse -force -ErrorAction SilentlyContinue | where {($_.CreationTime -lt  (Get-Date).AddDays($days) )} )| Remove-Item -Verbose -Force -Recurse -ErrorAction SilentlyContinue
+ 
+
+###############################################################
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Powershell-NPS-Backup
 NPS Backup Powershell for Server 2012 and up
+
+Usage Example
+.\Backup-Nps.ps1 -BackupDest "Path"
+
+**Path accepts both UNC or regular drive path"
+
+.\Backup-Nps.ps1 -BackupDest \\remotepath
+
+This script will export the NPS Configuration to the desired path and put a date stamp on it.
+
+Also it will check and remove files over than 7 days in the path.
